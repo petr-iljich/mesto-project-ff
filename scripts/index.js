823d6c68722a9dc0f7054bd969db06434c11ea8b
Petr Iljich <petr.ilj1ch@yandex.ru>
2025-05-01 13:25:30 +0300

    ПР Место

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(name, link) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция создания карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  const cardTitle = cardElement.querySelector('.card__title');
 
-// @todo: Функция удаления карточки
+  cardImage.src = link;
+  cardTitle.textContent = name;
 
-// @todo: Вывести карточки на страницу
+  deleteButton.addEventListener('click', () => {
+    const cardElement = deleteButton.closest('.places__item');
+    cardElement.remove();
+  });
+
+  return cardElement;
+}
+
+initialCards.forEach(card => {
+  const cardElement = createCard(card.name, card.link);
+  placesList.append(cardElement);
+});
